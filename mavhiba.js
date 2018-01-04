be012a4f54ec3d8f71985857e5575545f1fda338
nagygabor2 <nagy.gabor@gmx.net>
2018-01-04 14:29:24 +0100

    alpha#2

--- mavhiba.js
+++ mavhiba.js
@@ -1,23 +1,25 @@
-var template = "Tisztelt Ügyfélszolgálat!%0D%0A%0D%0APanaszt szeretnék tenni az alábbi járattal történt utazásommal kapcsolatban, mivel [complaint].%0D%0AInduló állomás: [origin]%0D%0ACélállomás: [destination]%0D%0AUtazás ideje: [schedule]%0D%0A%0D%0AKérem panaszomat kivizsgálni szíveskedjenek!%0D%0A%0D%0AÜdvözlettel: %0D%0A[customer_name]";
+var template = "Tisztelt Ügyfélszolgálat!%0D%0A%0D%0APanaszt szeretnék tenni az alábbi járattal történt utazásommal kapcsolatban,  [complaint]%0D%0A%0D%0AInduló állomás: [origin]%0D%0ACélállomás: [destination]%0D%0AUtazás ideje: [schedule]%0D%0A%0D%0AKérem panaszomat kivizsgálni szíveskedjenek!%0D%0A%0D%0AÜdvözlettel: %0D%0A[customer_name]";
 
 
-var messages = {delay:"a vonat jelentős késéssel közlekedett", canceled: "a vonat kimaradt, nem közlekedett", crowded:"a vonat rendkívül zsúfolt volt", noheating:"a vonaton a hűvös időjárás ellenére nem volt fűtés", noac:"a vonaton a rendkívül meleg időjárás ellenére nem üzemelt a klímaberendezés", toohighac:"a vonaton a klímaberendezés extrém alacsony hőmérsékleten működött", kidnapped:"a vonat ajtaja nem nyílt ki és a vonaton ragadtam",cantgeton:"nem tudtam felszállni a vonatra a rendkívüli zsúfoltság miatt"};
+var messages = {delay:"mivel a vonat jelentős késéssel közlekedett.", canceled: "mivel a vonat kimaradt, nem közlekedett.", crowded:"mivel a vonat rendkívül zsúfolt volt.", noheating:"mivel a vonaton a hűvös időjárás ellenére nem volt fűtés.", noac:"mivel a vonaton a rendkívül meleg időjárás ellenére nem üzemelt a klímaberendezés.", toohighac:"mivel a vonaton a klímaberendezés extrém alacsony hőmérsékleten működött.", kidnapped:"mivel a vonat ajtaja nem nyílt ki és a vonaton ragadtam.",cantgeton:"mivel nem tudtam felszállni a vonatra a rendkívüli zsúfoltság miatt.", other:"az alábbi problémák miatt:%0D%0A"};
 
 var recepient = "eszrevetel@mav-start._hu_";
 var request;
 
 $(document).ready(function() {
   
-    $('#schedule').bootstrapMaterialDatePicker({ switchOnClick: true, format : 'YYYY.MMMM.DD dddd - HH:mm', lang : 'hu', weekStart : 1});
+    $('#schedule').bootstrapMaterialDatePicker({ switchOnClick: true, format : 'YYYY.MM.DD, dddd - HH:mm', lang : 'hu', weekStart : 1});
 
   $("#submitbtn").click(function(index){  
-      var complaint = messages[$( "#complaint" ).val()];
+	  var complaint_orig = $( "#complaint" ).val();
+      var complaint = messages[complaint_orig];
       var schedule = $("#schedule").val();
       var origin = $("#origin").val();
       var destination = $("#destination").val();
       var customer_n = $("#customer_name").val();
+      var comment = $("#comment").val();
     
-    	if(origin == destination){
+    if(origin == destination){
         $('#error-div').text("Az induló- és célállomás ugyanaz!");
         $('#error-div').show();
         return;
@@ -34,15 +36,21 @@ $(document).ready(function() {
         $('#error-div').show();
         return;
       }
+	  
+	  if((complaint_orig == "other") && (comment == "")){
+        $('#error-div').text("A hiba részleteinek leírása kötelező!");
+        $('#error-div').show();
+        return;
+	  }
     
      $('#error-div').hide();
-      var message = template.replace("[complaint]", complaint).replace("[origin]", origin).replace("[destination]", destination).replace("[schedule]", schedule).replace("[customer_name]", customer_n);
+      var message = template.replace("[complaint]", complaint + "%0D%0A" + comment).replace("[origin]", origin).replace("[destination]", destination).replace("[schedule]", schedule).replace("[customer_name]", customer_n);
       window.open("mailto:" + recepient + "?subject=Panasz&body=" + message, "_blank");
 
 		request = $.ajax({
 			url: "https://script.google.com/macros/s/AKfycbxFTCEc7kmocjjvTaHuXzfPxBaL0fZMXA8c1M72SYj0wyYgspA/exec",
 			type: "GET",
-			data: "customer_n=" + customer_n + "&complaint=" + complaint + "&origin=" + origin + "&destination=" + destination + "&schedule=" + schedule
+			data: "customer_n=" + customer_n + "&complaint=" + complaint + "&origin=" + origin + "&destination=" + destination + "&schedule=" + schedule + "&comment=" + comment
 		});
 
 		// Callback handler that will be called on failure
